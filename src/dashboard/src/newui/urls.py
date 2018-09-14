2dbcf2823347d7699d2dd6d16b4f054aeee3831d
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2018-09-14 16:16:22 +0000

    Add (mostly unstyled) transfer page

--- src/dashboard/src/newui/urls.py
+++ src/dashboard/src/newui/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.dashboard, name='dashboard'),
+    url(r'^transfers/$', views.transfers, name='transfers'),
 ]
--- src/dashboard/src/newui/views.py
+++ src/dashboard/src/newui/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, 'newui/dashboard.html', {})
+
+def transfers(request):
+    return render(request, 'newui/transfers.html', {})
